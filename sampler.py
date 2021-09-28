3f73fa9851af064fe65447487edbbdf3ef63e6b0
PENGUINLOVEKOALA <wy17892022@gmail.com>
2021-09-28 11:41:51 +0900

    penguin

--- sampler.py
+++ sampler.py
@@ -47,7 +47,18 @@ class Sampler:
         
         return doc_name2data
 
-    def count_number_of_tags_and_values(self):
+    def frequency_measure_tag_and_values(self):
+        """
+        This functions print out the info on tag and value frequencies in the CLI terminal 
+        and save the info into the txt files within the working directory.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+
         doc_name2data = self.load_data()
 
         tag_dict = dict()
@@ -58,13 +69,39 @@ class Sampler:
                 tags = [x['tag'] for x in basket['tag_value_list']]
                 values = [x['value'] for x in basket['tag_value_list']]
                 for tag_ in tags:
-                    tag_dict[tag_] = True
+                    if tag_ in tag_dict:
+                        tag_dict[tag_] += 1
+                    else:
+                        tag_dict[tag_] = 1
                 for value_ in values:
-                    value_dict[value_] = True
+                    if value_ in value_dict:
+                        value_dict[value_] += 1
+                    else:
+                        value_dict[value_] = 1
 
         tag_list = list(tag_dict.keys())
         value_list = list(value_dict.keys())
 
+        tag_list = sorted(tag_list, key = lambda x: tag_dict[x], reverse=True)
+        value_list = sorted(value_list, key = lambda x: value_dict[x], reverse=True)
+        
+        tag_save_text = ""
+        value_save_text = ""
+        
+        for tag_ in tag_list:
+            tag_save_text += "\t".join([tag_, str(tag_dict[tag_])])
+            tag_save_text += "\n"
+
+        for value_ in value_list:
+            value_save_text += "\t".join([value_, str(value_dict[value_])]) 
+            value_save_text += "\n"
+
+        tag_saver = TEXTSaver(tag_save_text, f"tag_frequency_{self.f_tail}.txt") 
+        tag_saver()
+        value_saver = TEXTSaver(value_save_text, f"value_frequency_{self.f_tail}.txt") 
+        value_saver()
+
+        print("[File Saved] tag and value frequency list")
         print("-"*30)
         print(self.f_data)
         print("-"*30)
@@ -74,6 +111,12 @@ class Sampler:
         print('#values:', len(value_list))
         print('value_sampes:', value_list[:10])
         print("-"*30)
+
+        return
+
+    def intersection_between_text_and_tag(self):
+        # text = 
+
 
         return
 
@@ -192,5 +235,5 @@ def parlai_data_create():
 if __name__ == "__main__":
     for data_type in ['train', 'dev', 'test']:
         sampler = Sampler(data_type)
-        sampler.count_number_of_tags_and_values()
+        sampler.frequency_measure_tag_and_values()
 
